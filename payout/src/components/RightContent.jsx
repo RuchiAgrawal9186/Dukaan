6f7cfebdf25150fcfd56918b92c4e69e46ce8d4c
Ruchi <ruchiagrawal9186@gmail.com>
2023-12-23 12:45:44 +0530

    payout page design is done

--- payout/src/components/RightContent.jsx
+++ payout/src/components/RightContent.jsx
@@ -1,82 +1,100 @@
-import React from 'react'
-import "./RightContent.css"
-import Table from './Table'
+import React from "react";
+import "./RightContent.css";
+import Table from "./Table";
 
 const RightContent = () => {
   return (
     <>
-    <div className='rightcontainer'>
-      <div className='topdiv'>
-
-        <div style={{display:"flex",gap:"10px"}}>
-          <h5>Payouts</h5>
-          <div>How it works</div>
-        </div>
-
-        <div className='searchdiv' style={{display:"flex",gap:"5px"}}>
-          <div> <img src="./images/search.svg" alt="" /></div>
-          <input type="text" placeholder='Search features, tutorials, etc.'/>
-        </div>
-
-        <div style={{display:"flex",gap:"10px"}}>
-          <div><img src="./images/Menu.png" alt="" /></div>
-          <div><img src="./images/Menu1.svg" alt="" /></div>
-        </div>
-
-      </div>
-
-      <div className='overview'>
-
-        <div className='top'>
-          <h4>Overview</h4>
-          <div>
-            <select name="" id="">
-              <option value="">This Month</option>
-            </select>
+      <div className="rightcontainer">
+        <div className="topdiv">
+          <div style={{ display: "flex", gap: "10px" }}>
+            <h5>Payouts</h5>
+            <div>How it works</div>
           </div>
-        </div>
 
-        <div className='gridDiv'>
-          <div className='first'>
-            <p>Next Payout</p>
-            <div style={{display:"flex",justifyContent:"space-between"}}>
-              <div> <h2>₹2,312.23</h2></div>
-             <div> <p>23 orders</p></div>
+          <div className="searchdiv" style={{ display: "flex", gap: "5px" }}>
+            <div>
+              {" "}
+              <img src="./images/search.svg" alt="" />
             </div>
-            <div className='btm' >
-              <p>Next payout date:</p>
-              <p>Today, 04:00PM</p>
+            <input type="text" placeholder="Search features, tutorials, etc." />
+          </div>
+
+          <div style={{ display: "flex", gap: "10px" }}>
+            <div>
+              <img src="./images/Menu.png" alt="" />
+            </div>
+            <div>
+              <img src="./images/Menu1.svg" alt="" />
             </div>
           </div>
-          <div className='second'>
+        </div>
+
+        <div className="overview">
+          <div className="top">
+            <h4>Overview</h4>
+            <div>
+              <select name="" id="">
+                <option value="">This Month</option>
+              </select>
+            </div>
+          </div>
+
+          <div className="gridDiv">
+            <div className="first">
+              <p>Next Payout</p>
+              <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <div>
+                  {" "}
+                  <h2>₹2,312.23</h2>
+                </div>
+                <div>
+                  {" "}
+                  <p>23 orders</p>
+                </div>
+              </div>
+              <div className="btm">
+                <p>Next payout date:</p>
+                <p>Today, 04:00PM</p>
+              </div>
+            </div>
+            <div className="second">
               <p>Amount Pending</p>
-              <div style={{display:"flex",justifyContent:"space-between"}}>
-              <div> <h2>₹92,312.20</h2></div>
-             <div> <p>13 orders</p></div>
+              <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <div>
+                  {" "}
+                  <h2>₹92,312.20</h2>
+                </div>
+                <div>
+                  {" "}
+                  <p style={{ color: "blue" }}>13 orders</p>
+                </div>
+              </div>
+            </div>
+            <div className="third">
+              <p>Amount Pending</p>
+              <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <div>
+                  {" "}
+                  <h2>₹92,312.20</h2>
+                </div>
+                {/* <div> <p>13 orders</p></div> */}
+              </div>
             </div>
           </div>
-          <div className='third'>
-          <p>Amount Pending</p>
-              <div style={{display:"flex",justifyContent:"space-between"}}>
-              <div> <h2>₹92,312.20</h2></div>
-             <div> <p>13 orders</p></div>
-            </div>
-          </div>
-        </div>
 
-        <div className='tablediv'>
+          <div className="tablediv">
             <h4>Transactions | This Month</h4>
-            <div style={{display:"flex",gap:"10px"}}>
+            <div style={{ display: "flex", gap: "10px" }}>
               <button>Payouts (22)</button>
               <button>Refunds (2)</button>
             </div>
-          <Table></Table>
+            <Table></Table>
+          </div>
         </div>
       </div>
-
-    </div>
     </>
-  )
-}
+  );
+};
 
-export default RightContent
+export default RightContent;
--- payout/src/components/Sidebar.jsx
+++ payout/src/components/Sidebar.jsx
@@ -1,95 +1,105 @@
-import React from 'react'
-import "./Sidebar.css"
+import React from "react";
+import "./Sidebar.css";
 
 const Sidebar = () => {
   return (
     <>
-    <div className='leftcontainer'>
-        <div style={{display:"flex",gap:"10px"}}>
-            <div>
-            <img src="./images/image.png" alt="" style={{width:"100%"}}/>
-            </div>
+      <div className="leftcontainer">
+        <div style={{ display: "flex", gap: "10px" }}>
+          <div>
+            <img src="./images/image.png" alt="" style={{ width: "100%" }} />
+          </div>
 
-            <div style={{display:"flex",flexDirection:"column"}}>
+          <div style={{ display: "flex", flexDirection: "column" }}>
             <div>Nishyan</div>
             <div>Visit store</div>
-            </div>
+          </div>
 
-            <div>
-                <img src="./images/Chevron Down.png" alt=""  style={{width:"100%"}}/>
-            </div>
+          <div>
+            <img
+              src="./images/Chevron Down.png"
+              alt=""
+              style={{ width: "100%" }}
+            />
+          </div>
         </div>
 
-
-        <div className='icons'>
-        <div className='iconscontainer'>
+        <div className="icons">
+          <div className="iconscontainer">
             <img src="./images/Navbar Icon.png" alt="" />
             <p>Home</p>
-        </div>
+          </div>
 
-        <div className='iconscontainer'>
+          <div className="iconscontainer">
             <img src="./images/Navbar Icon1.png" alt="" />
             <p>Orders</p>
-        </div>
+          </div>
 
-        <div className='iconscontainer'>
+          <div className="iconscontainer">
             <img src="./images/Navbar Icon2.png" alt="" />
             <p>Products</p>
-        </div>
+          </div>
 
-        <div className='iconscontainer'>
+          <div className="iconscontainer">
             <img src="./images/Navbar Icon3.png" alt="" />
             <p>Delivery</p>
-        </div>
+          </div>
 
-        <div className='iconscontainer'>
+          <div className="iconscontainer">
             <img src="./images/Navbar Icon4.png" alt="" />
             <p>Marketing</p>
-        </div>
+          </div>
 
-        <div className='iconscontainer'>
+          <div className="iconscontainer">
             <img src="./images/Navbar Icon5.png" alt="" />
             <p>Analytics</p>
-        </div>
+          </div>
 
-        <div className='iconscontainer'>
+          <div className="iconscontainer">
             <img src="./images/Navbar Icon6.png" alt="" />
             <p>Payouts</p>
-        </div>
+          </div>
 
-        <div className='iconscontainer'>
+          <div className="iconscontainer">
             <img src="./images/Navbar Icon7.png" alt="" />
             <p>Discounts</p>
-        </div>
-        <div className='iconscontainer'>
+          </div>
+          <div className="iconscontainer">
             <img src="./images/Navbar Icon8.png" alt="" />
             <p>Audience</p>
-        </div>
-        <div className='iconscontainer'>
-            <div><img src="./images/Navbar Icon9.png" alt="" /></div>
-            <div> <p>Appearance</p></div>
-           
-        </div>
-
-        <div className='iconscontainer'>
-            <div><img src="./images/Navbar Icon10.png" alt="" /></div>
-            <div><p>Plugins</p></div>
-            
-        </div>
-
-        </div>
-
-
-        <div style={{display:"flex",gap:"10px"}} className='btmnav'>
-            <div><img src="./images/wallet.svg" alt="" /></div>
-            <div style={{display:"flex",flexDirection:"column"}}>
-               <div>Available credits</div>
-               <div>222.10</div>
+          </div>
+          <div className="iconscontainer">
+            <div>
+              <img src="./images/Navbar Icon9.png" alt="" />
             </div>
-        </div>
-    </div>
-    </>
-  )
-}
+            <div>
+              {" "}
+              <p>Appearance</p>
+            </div>
+          </div>
 
-export default Sidebar
+          <div className="iconscontainer">
+            <div>
+              <img src="./images/Navbar Icon10.png" alt="" />
+            </div>
+            <div>
+              <p>Plugins</p>
+            </div>
+          </div>
+        </div>
+
+        <div style={{ display: "flex", gap: "10px" }} className="btmnav">
+          <div>
+            <img src="./images/wallet.svg" alt="" />
+          </div>
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <div>Available credits</div>
+            <div>222.10</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Sidebar;
